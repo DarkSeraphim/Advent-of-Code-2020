a391cc7b1d46acf75cc0da76a352cf09c63a6fbf
Mark Hendriks <mark.hendriks@hybrit.org>
2020-12-01 12:54:17 +0100

    Rename :done to :halt, and wrap part in a function rather than relying on variables

--- day-1/part-2.exs
+++ day-1/part-2.exs
@@ -2,14 +2,14 @@ solve_for_n = fn(elements, target) ->
   elements
   |> Enum.reduce_while(%{}, fn(num, req) ->
     if Map.has_key?(req, num) do 
-      {:halt, {:done, Map.get(req, num) * num}}
+      {:halt, {:halt, Map.get(req, num) * num}}
     else
       {:cont, Map.put(req, target - num, num)}
     end
   end)
 end
 
-part = File.read!("day-1/input.txt")
+File.read!("day-1/input.txt")
 |> String.trim_trailing()
 |> String.split("\n")
 |> Enum.map(&Kernel.elem(Integer.parse(&1, 10), 0))
@@ -17,17 +17,18 @@ part = File.read!("day-1/input.txt")
     Map.put(req, 2020 - num, num)
   end)
 
-part
-|> Map.to_list()
-|> Enum.reduce_while(nil, fn({left, first}, nop) ->
-    Map.values(part)
-    |> Enum.filter(&(&1 != first))
-    |> solve_for_n.(left)
-    |> case do
-        {:done, value} -> {:halt, value * first}
-        _ -> {:cont, nop}
-      end
-  end)
-|> IO.puts
-
+|> fn(part) ->
+  part
+  |> Map.to_list()
+  |> Enum.reduce_while(nil, fn({left, first}, nop) ->
+      Map.values(part)
+      |> Enum.filter(&(&1 != first))
+      |> solve_for_n.(left)
+      |> case do
+          {:halt, x} -> {:halt, x * first}
+          _ -> {:cont, nop}
+        end
+    end)
+  |> IO.puts
+end.()
 
