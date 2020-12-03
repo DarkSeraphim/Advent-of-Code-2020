25786f3ec4ab372186a1caac05e53630d854c1fc
Mark Hendriks <mark.hendriks@hybrit.org>
2020-12-03 10:19:28 +0100

    Solve day 3, now with 100% more elixir modules

---
+++ day-3/part-1.exs
@@ -0,0 +1,25 @@
+defmodule Day3_1 do
+  defp check_tree({column, treeRow}) do
+    Enum.at(treeRow, Kernel.rem(column, Enum.count(treeRow)))
+  end
+
+  defp count_trees(trees) do
+    0..Enum.count(trees)
+    |> Enum.map(&Kernel.*(&1, 3))
+    |> Enum.zip(trees)
+    |> Enum.map(&check_tree/1)
+    |> Enum.filter(&(&1 == "#"))
+    |> Enum.count()
+  end
+
+  def solve() do
+    IO.read(:stdio, :all)
+    |> String.trim_trailing()
+    |> String.split("\n")
+    |> Enum.map(&(String.split(&1, "", trim: true)))
+    |> count_trees()
+    |> IO.puts()
+  end
+end
+
+Day3_1.solve()
---
+++ day-3/part-2.exs
@@ -0,0 +1,39 @@
+defmodule Day3_2 do
+  defp check_tree({column, row}, trees) do
+    if row < Enum.count(trees) do
+      treeRow = Enum.at(trees, row)
+      Enum.at(treeRow, Kernel.rem(column, Enum.count(treeRow)))
+    else
+      '.'
+    end
+  end
+
+  defp count_trees(trees, {step_col, step_row}) do
+    0..Enum.count(trees)
+    |> Enum.map(&Kernel.*(&1, step_col))
+    |> Enum.zip(
+      0..ceil(Enum.count(trees) / step_row)
+      |> Enum.map(&Kernel.*(&1, step_row))
+    )
+    |> Enum.map(&(check_tree(&1, trees)))
+    |> Enum.filter(&(&1 == "#"))
+    |> Enum.count()
+  end
+
+  def solve_for_set(trees) do
+    [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+    |> Enum.map(&(count_trees(trees, &1)))
+    |> Enum.reduce(&(&1 * &2))
+  end
+
+  def solve() do
+    IO.read(:stdio, :all)
+    |> String.trim_trailing()
+    |> String.split("\n")
+    |> Enum.map(&(String.split(&1, "", trim: true)))
+    |> solve_for_set()
+    |> IO.puts()
+  end
+end
+
+Day3_2.solve()
