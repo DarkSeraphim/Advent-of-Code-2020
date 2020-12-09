c5ab3645d46c2fd93d24ba16afc4782b8a343bed
Mark Hendriks <mark.hendriks@hybrit.org>
2020-12-09 12:55:18 +0100

    Clean up runner module

--- lib/runner.ex
+++ lib/runner.ex
@@ -2,11 +2,8 @@ defmodule Runner do
   use Application
   
   def start(_type, _args) do
-    args = System.argv()
-    day = Enum.at(args, 1)
-    part = Enum.at(args, 2)
-    target_name = "Elixir.Day#{day}_#{part}"
-    apply(String.to_atom(target_name), :solve, [])
+    [_, day, part | _] = System.argv()
+    apply(String.to_atom("Elixir.Day#{day}_#{part}"), :solve, [])
     {:ok, self()}
   end
 
