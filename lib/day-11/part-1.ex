7f338d378d37ce2251cc3358970f406805bdeeea
Mark Hendriks <mark.hendriks@hybrit.org>
2020-12-11 11:25:34 +0100

    Add Memoize (but don't use it yet), and push day 11

---
+++ lib/day-11/part-1.ex
@@ -0,0 +1,50 @@
+defmodule Day11_1 do
+  def pair_with_idx(list), do: Enum.zip(0..Enum.count(list), list)
+
+  def split_row({y, list}) do
+    list = String.graphemes(list)
+    for {x, seat} <- pair_with_idx(list), seat != ".", do: {{x, y}, seat}
+  end
+
+  def add_delta({dx, dy}, {x, y}), do: {dx + x, dy + y}
+
+  def count_filled(coords, map) do
+    (for dx <- -1..1, dy <- -1..1, {dx,dy} != {0,0}, do: {dx, dy}
+      |> add_delta(coords)
+      |> (&Map.get(map, &1)).()
+      |> Kernel.==("#"))
+    |> Enum.filter(&(&1))
+    |> Enum.count()
+  end
+
+  # Do one cycle, and return new Map
+  def cycle(map) do
+    (for {coords, seat} <- map, do: count_filled(coords, map)
+    |> (case do
+      0 when seat == "L" -> {coords, "#"};
+      x when seat == "#" and x >= 4 -> {coords, "L"};
+      _ -> {coords, seat};
+    end))
+    |> Map.new()
+  end
+
+  def game_of_life?(map) do
+    new_map = cycle(map)
+    if map == new_map do
+      Map.values(map)
+      |> Enum.frequencies()
+      |> Map.get("#")
+    else
+      game_of_life?(new_map)
+    end
+  end
+
+  def solve() do
+    Common.read_lines()
+    |> pair_with_idx()
+    |> Enum.flat_map(&split_row/1)
+    |> Map.new()
+    |> game_of_life?()
+    |> IO.inspect()
+  end
+end
--- lib/lib.ex
+++ lib/lib.ex
@@ -6,4 +6,15 @@ defmodule Common do
   end
 
   def int(s), do: String.to_integer(s)
+
+  def inspect_grid_row(row, map, cols \\ 10) do
+    Enum.map(0..(cols-1), fn(col) -> Map.get(map, {col, row}, ".") end)
+    |> Enum.join("")
+  end
+
+  def inspect_grid(map, cols \\ 10, rows \\ 10) do
+    IO.puts(Enum.map(0..(rows-1), (&(inspect_map_row(&1, map, cols))))
+    |> Enum.join("\n"))
+    map
+  end
 end
